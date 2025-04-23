46a9bbae75fc951384563c120e9779f17ef48d04
José <jcuencacoronado@gmail.com>
2025-04-23 17:24:09 -0400

    Refactorización de AppHub para cargar módulos desde cookies. Se eliminaron dependencias estáticas y se agregó lógica dinámica en helpers

---
+++ src/helpers/cookies.ts
@@ -0,0 +1,19 @@
+import { cookies } from "next/headers";
+
+import { Module } from "./interfaces";
+
+export async function getModules() {
+  const cookieStore = await cookies();
+  const raw = cookieStore.get("modules")?.value;
+
+  if (!raw) return [];
+  try {
+    const modules: Module[] = JSON.parse(raw);
+
+    return modules;
+  } catch (err) {
+    console.error("Error al parsear la cookie 'modules':", err);
+
+    return [];
+  }
+}
