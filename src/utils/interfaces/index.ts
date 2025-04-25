e0d4d50f6dc55052e9aa44d3187acf4e2ee314a3
José <jcuencacoronado@gmail.com>
2025-04-25 16:44:19 -0400

    Se agrego interfaces User, ResponseData y Role

--- src/utils/interfaces/index.ts
+++ src/utils/interfaces/index.ts
@@ -5,3 +5,20 @@ export interface Module {
   urlDev: string;
   urlManual: string;
 }
+export interface User {
+  id: string;
+  name: string;
+  username: string;
+  identityCard?: string;
+  position?: string;
+}
+export interface ResponseData {
+  error: boolean;
+  message: string;
+  [key: string]: any;
+}
+
+export interface Role {
+  id: string;
+  name: string;
+}
