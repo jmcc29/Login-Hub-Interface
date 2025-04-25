4f7fd74b44f3e95d4c1100191456946e81b6659b
José <jcuencacoronado@gmail.com>
2025-04-25 16:48:33 -0400

    Se creo funcion para leer la cookie user

--- src/utils/helpers/cookies.ts
+++ src/utils/helpers/cookies.ts
@@ -1,8 +1,27 @@
 import { cookies } from "next/headers";
 
-import { Module } from "@/utils/interfaces/index";
+import { Module, ResponseData, User } from "@/utils/interfaces/index";
+import { error } from "console";
+async function getCookie(name: string) {
+  const cookieStore = await cookies();
+  const cookie = cookieStore.get(name)?.value;
+  return cookie;
+}
+function parseCookie(raw: string) {
+  try {
+    return JSON.parse(raw);
+  } catch (error) {
+    console.error("Error al parsear la cookie:", error);
+    return null;
+  }
+}
+export async function getUserCookie() {
+  const raw = await getCookie("user");
+  if (!raw) return [];
+  return parseCookie(raw);
+}
 
-export async function getModules() {
+export async function getModulesCookie() {
   const cookieStore = await cookies();
   const raw = cookieStore.get("modules")?.value;
 
