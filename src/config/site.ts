cf41ccbbd3bb9b9fd49380cac5e97e95d0369028
ferbautistamollo <ferbautistamollo@gmail.com>
2025-05-02 09:49:04 -0400

    comflictos

---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export type ComputerTools = typeof computerTools;
+
+// TODO debería ser una API
+export const computerTools = [
+  {
+    name: "BENEFICIARIOS",
+    subtitle: "Herramienta Informático",
+    url_prod: "http://192.168.2.240:3002/persons",
+    url_dev: "http://192.168.2.240:3002/persons",
+    url_manual: "http://192.168.2.240:3002/persons",
+    image: "beneficiary.jpg",
+  },
+];
