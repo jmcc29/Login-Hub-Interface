d8adaea8e455d48c11fa7f28abbf7b805461fc85
José <jcuencacoronado@gmail.com>
2025-05-08 15:31:48 -0400

    Se agrega footer para Version de pruebas en layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import clsx from "clsx";
 import { Providers } from "./providers";
 
 import { fontSans } from "@/utils/fonts";
+import { getDeployEnvironment } from "@/utils/envs";
 
 export const viewport: Viewport = {
   themeColor: [
@@ -16,24 +17,32 @@ export const metadata = {
   icons: {
     icon: "/icono_muserpol.svg",
   },
-}
+};
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  console.log("layout" + getDeployEnvironment());
   return (
     <html suppressHydrationWarning lang="en">
       <head />
       <body
         className={clsx(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable,
+          fontSans.variable
         )}
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
           <div className="relative flex flex-col h-screen">
             <main className="w-full flex-grow">{children}</main>
+            {getDeployEnvironment() === "dev" && (
+              <footer className="fixed bottom-0 left-0 w-full bg-red-600 text-white text-center py-2 text-sm z-50">
+                <span className="uppercase text-sm font-semibold">
+                  Versión de pruebas
+                </span>
+              </footer>
+            )}
           </div>
         </Providers>
       </body>
