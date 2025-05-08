dd201142a35ea7d752380a4a93dfb79ce4e9a1ad
José <jcuencacoronado@gmail.com>
2025-05-08 15:29:35 -0400

    Archivo nuevo envs dentro de utils, con funcion para env de despliegue

---
+++ src/utils/envs.ts
@@ -0,0 +1,5 @@
+export function getDeployEnvironment(): "dev" | "prod" | "test" {
+    const env = process.env.NEXT_PUBLIC_DEPLOY_ENV;
+    if (env === "prod" || env === "test" || env === "dev") return env;
+    return "prod";
+  }
