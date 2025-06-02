1bd25097b444d50f22d73979bf36e41e415b2742
José <jcuencacoronado@gmail.com>
2025-06-02 16:20:54 -0400

    Actualizacion de interfaces y types

--- src/utils/types/index.ts
+++ src/utils/types/index.ts
@@ -1,5 +1,8 @@
 import { SVGProps } from "react";
 
+import { Module } from "@/utils/interfaces";
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
   size?: number;
 };
+
+export type ModulePartial = Partial<Module>;
