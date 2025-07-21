ed0906c58208cb10222a33a1ae48c86a5e714906
ferbautistamollo <ferbautistamollo@gmail.com>
2025-07-21 13:17:24 -0400

    AJUSTE  DE COMPONENTE UTILIZADO, SOLO SE USA UNA URL

--- src/app/apphub/page.tsx
+++ src/app/apphub/page.tsx
@@ -1,18 +1,23 @@
-import { computerTools } from "@/config/site";
-import Software from "@/components/software";
-
+import Software from "@/components/apphub/software";
 export default function AppHub() {
+  const tools = [
+    {
+      name: "BENEFICIARIOS",
+      subtitle: "HERRAMIENTA INFORMÁTICA",
+      url: `http://${process.env.NEXT_PUBLIC_SERVER_FRONTEND || "localhost"}:3002/persons`,
+      image: "beneficiary.jpg",
+    },
+  ];
+
   return (
-    <div className="max-w-full gap-7 grid grid-cols-8 px-10 py-5">
-      {(computerTools ?? []).map((computerTool, index) => (
+    <div className="max-w-full gap-7 grid grid-cols-8 p-5">
+      {tools.map((computerTool, index) => (
         <Software
           key={index}
           image={computerTool.image}
           name={computerTool.name}
           subtitle={computerTool.subtitle}
-          url_dev={computerTool.url_dev}
-          url_manual={computerTool.url_manual}
-          url_prod={computerTool.url_prod}
+          url={computerTool.url}
         />
       ))}
     </div>
