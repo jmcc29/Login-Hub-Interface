3aecfc7ad46848eaaa15e53972413d526a117d09
José <jcuencacoronado@gmail.com>
2025-09-12 01:01:27 -0400

    Login Keycloak, con rutas login y callback, middleware refactorizado para verificar sid

---
+++ src/app/api/auth/callback/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getBackendUrl } from "@/utils/env";
+import { apiClient, apiServerFrontend } from "@/utils/services";
+
+export async function GET(req: NextRequest) {
+  const backendBase = getBackendUrl();
+  const url = new URL(req.url);
+  const code = url.searchParams.get("code");
+  const state = url.searchParams.get("state");
+
+  if (!code || !state) {
+    return NextResponse.json({ error: "Faltan code/state" }, { status: 400 });
+  }
+
+  const r= await apiClient.POST("auth/exchange", {
+    code, state
+  })
+  if (!r.ok) {
+    const err = await r.text();
+    return NextResponse.json({ error: "exchange_failed", detail: err }, { status: 401 });
+  }
+
+  const { sessionId, returnTo } = await r.json();
+
+  const resp = NextResponse.redirect(returnTo || "/modules", { status: 302 });
+  resp.cookies.set({
+    name: "sid",
+    value: sessionId,
+    httpOnly: true,
+    sameSite: "lax",   
+    secure: false,        // ponlo en true en producción (HTTPS)
+    path: "/",
+    maxAge: 60 * 60 * 8,  // 8h
+  });
+  return resp;
+}
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -5,3 +5,13 @@ export function getDeployEnvironment(): "dev" | "prod" | "test" {
 
   return "prod";
 }
+
+export function getBackendUrl(): string {
+  const url = "http://" + process.env.NEXT_PUBLIC_BACKEND_HOST + ":" + process.env.NEXT_PUBLIC_BACKEND_PORT;
+  return url;
+}
+
+export function getFrontendUrl(): string {
+  const url = "http://" + process.env.NEXT_PUBLIC_SERVER_FRONTEND + ":" + process.env.NEXT_PUBLIC_SERVER_PORT_FRONTEND;
+  return url;
+}
