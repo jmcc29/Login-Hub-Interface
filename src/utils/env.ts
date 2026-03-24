5d1698787f99cd7fcea2ee74466f1d79f5235908
José <jcuencacoronado@gmail.com>
2026-03-24 19:25:04 -0400

    chore(config): actualizar variables de entorno y exportar objetos de configuración de frontends

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -5,3 +5,26 @@ export function getDeployEnvironment(): "dev" | "prod" | "test" {
 
   return "prod";
 }
+
+export const backend = {
+  host: process.env.NEXT_PUBLIC_BACKEND_HOST,
+  port: process.env.NEXT_PUBLIC_BACKEND_PORT,
+  url: `http://${process.env.NEXT_PUBLIC_BACKEND_HOST || "localhost"}:${process.env.NEXT_PUBLIC_BACKEND_PORT || "3000"}`,
+};
+
+export const frontend = {
+  host: process.env.NEXT_PUBLIC_HUB_FRONTEND_HOST,
+  port: process.env.NEXT_PUBLIC_HUB_FRONTEND_PORT,
+  url: `http://${process.env.NEXT_PUBLIC_HUB_FRONTEND_HOST || "localhost"}:${process.env.NEXT_PUBLIC_HUB_FRONTEND_PORT || "3001"}`,
+  clientId: process.env.NEXT_PUBLIC_HUB_CLIENT_ID || "hub-interface",
+};
+
+export const externalFrontends = {
+  beneficiary: {
+    host: process.env.NEXT_PUBLIC_BENEFICIARY_FRONTEND_HOST,
+    port: process.env.NEXT_PUBLIC_BENEFICIARY_FRONTEND_PORT,
+    url: `http://${process.env.NEXT_PUBLIC_BENEFICIARY_FRONTEND_HOST || "localhost"}:${process.env.NEXT_PUBLIC_BENEFICIARY_FRONTEND_PORT || "3002"}`,
+    clientId:
+      process.env.NEXT_PUBLIC_CLIENT_ID_BENEFICIARY || "beneficiary-interface",
+  },
+};
